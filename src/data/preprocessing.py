bac633515d2142277d5cc1988c803b440aea0f3b
swetlanas <swaru004@umn.edu>
2026-07-20 17:38:50 -0500

    added concept_hierarchy_graph() to parse rdf into NetworkX

--- src/data/preprocessing.py
+++ src/data/preprocessing.py
@@ -7,9 +7,12 @@ from pathlib import Path
 import pandas as pd
 import requests
 import re
+import networkx as nx
 import numpy as np
 from rdflib import Graph, Namespace
 import gzip,io
+import logging
+from functools import lru_cache
 
 ROOT = Path.cwd()
 while not (ROOT / ".git").exists():
@@ -17,8 +20,19 @@ while not (ROOT / ".git").exists():
 DATA = ROOT / "data" / "prb_headings_full.csv"
 
 URL = "https://raw.githubusercontent.com/physh-org/PhySH/master/physh.rdf.gz"
-
 SKOS = Namespace("http://www.w3.org/2004/02/skos/core#")
+
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s",handlers=[
+        logging.FileHandler("preprocessing.log"),
+        logging.StreamHandler()
+    ])
+logger = logging.getLogger(__name__)
+
+
+
+
+
 
 def concept_id_to_physh_name():
     """
@@ -40,6 +54,39 @@ def concept_id_to_physh_name():
                 uuid_to_label[uuid] = str(obj)
     
     return uuid_to_label
+
+def concept_hierarchy_graph():
+    """
+    Loads PhySH rdf from github and returns a NetworkX graph with concept ID hierarchy.
+    """
+
+    response = requests.get(URL, stream=True)
+    with gzip.open(io.BytesIO(response.content), "rt", encoding="utf-8") as f:
+            rdf_text = f.read()
+
+    g = Graph()
+    rdf_data = g.parse(data=rdf_text, format="xml")
+
+    nx_graph = nx.DiGraph()
+
+    # Build directional edges: Parent -> Child
+    for child,predicate, parent in g.triples((None, SKOS.broader, None)):
+        child_id = str(child)
+        parent_id = str(parent)
+        nx_graph.add_edge(parent_id, child_id)
+
+    # Build directional edges: Child -> Parent
+    for child,predicate, parent in g.triples((None, SKOS.narrower, None)):
+        child_id = str(child)
+        parent_id = str(parent)
+        nx_graph.add_edge(child_id, parent_id) #add_edge() automatically takes care of duplicates if a parent, child pair was already defined
+
+    # Add node labels 
+    for child,predicate, parent in g.triples((None, SKOS.prefLabel, None)):
+        if str(child) in nx_graph:
+            nx_graph.nodes[str(child)]['label'] = str(parent)
+    
+    return nx_graph
 
 def clean_abstract(df_raw):
     """
