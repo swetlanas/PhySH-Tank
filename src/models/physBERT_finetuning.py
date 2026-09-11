0bb72edabf3b0505e5fefab1dbafc6d865abdc70
swetlanas <swaru004@umn.edu>
2026-09-11 11:57:20 -0500

    Set up Huggingface trainers

--- src/models/physBERT_finetuning.py
+++ src/models/physBERT_finetuning.py
@@ -8,16 +8,14 @@ from pathlib import Path
 import pandas as pd
 import numpy as np
 from tqdm import tqdm
-from sklearn.preprocessing import MultiLabelBinarizer
-from sklearn.model_selection import train_test_split
 import torch
-import torch.nn as nn
-from torch.utils.data import DataLoader, TensorDataset
+
 
 #From HuggingFace
 from transformers import AutoTokenizer, AutoModelForSequenceClassification, DataCollatorWithPadding
 from transformers import Trainer,TrainingArguments
 from peft import get_peft_model, LoraConfig, TaskType, PeftModel
+from datasets import Dataset
 
 ROOT = Path.cwd()
 while not (ROOT / ".git").exists():
@@ -29,19 +27,46 @@ DATA_PATH = ROOT / "data"
 CLEANED_DATA  = DATA_PATH / "cleaned_data.json"
 CLEANED_DATA_SAMPLE  = DATA_PATH / "cleaned_data_sample.json"
 
-BATCH_SIZE = 32
+BATCH_SIZE = 16
 MODEL_NAME = "thellert/physbert_uncased"
 
 EPOCHS=10
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s",handlers=[
-        logging.FileHandler(ROOT / "logs" / "physbert_classifier.log"),
+        logging.FileHandler(ROOT / "logs" / "physbert_finetuning.log"),
         logging.StreamHandler()
     ])
 logger = logging.getLogger(__name__)
 
 
+def compute_metrics(eval_pred):
 
+    logits, labels = eval_pred
+    logits = torch.tensor(logits)
+    labels = torch.tensor(labels)
+
+    k = 5
+    _, topk_indices = torch.topk(logits, k=k, dim=1)
+    topk_targets = torch.gather(labels, dim=1, index=topk_indices)
+    precision_per_sample = topk_targets.sum(dim=1) / float(k)
+
+    return {"precision_at_5": precision_per_sample.mean().item()}
+
+
+def tokenize_batch(batch):
+    """
+    For providing batched tokens to huggingface trainer
+
+    Args:
+        batch : batch_size of the dataframe to be processed for training
+        
+    Returns:
+        tokenized batch
+    """
+    return tokenizer(batch["title"],
+                       batch["abstract"],
+                       truncation="only_second",
+                       max_length=512)
 
 if __name__=='__main__':
 
@@ -65,15 +90,22 @@ if __name__=='__main__':
     #Full HuggingFace dataset
     raw_dataset = Dataset.from_pandas(df[["title", "abstract", "labels"]])
 
-    #Split into train/test splits
+    #Split into train/test splits for huggingface TrainingArguments
     split_dataset = raw_dataset.train_test_split(test_size=0.2, seed=0)
+
+    #Prepare PhysBERT for tokenization
+    tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+
+    tokenized_train = split_dataset["train"].map(tokenize_batch, batched=True)
+    tokenized_test = split_dataset["test"].map(tokenize_batch, batched=True)
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     logger.info(f"Using compute device: {device}")
     
+    # Data collator for dynamic padding
+    collator = DataCollatorWithPadding(tokenizer)
 
-    # Load PhysBERT tokenizer and model
-    tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+    # Load PhysBERT for classification task training
     model_physbert = AutoModelForSequenceClassification.from_pretrained(MODEL_NAME,
                                                                         num_labels=num_classes,
                                                                         problem_type ="multi_label_classification")
@@ -84,76 +116,48 @@ if __name__=='__main__':
         r=8,
         lora_alpha=16,
         lora_dropout=0.1,
-        target_modules=["q_lin", "v_lin"],
+        target_modules=["query", "value"],
         modules_to_save = ["classifier"],
     )
 
     # Applying LoRA configuration to the distilbert model
     model_physbert = get_peft_model(model_physbert, lora_config)
+    logger.info(f"{model_physbert.print_trainable_parameters()}")
     model_physbert.to(device)
 
-    # Splitting the dataset into train and test sets for single label
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
-    # Create PyTorch Dataset and DataLoader
-    train_dataset = TensorDataset(X_train, y_train)
-    train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
+    training_args = TrainingArguments(
+        output_dir=str(DATA_PATH / "models" / "physbert_lora_finetuned"),
+        logging_dir = str(ROOT / "logs"),
+        num_train_epochs=EPOCHS,
+        per_device_train_batch_size=BATCH_SIZE,
+        per_device_eval_batch_size=BATCH_SIZE,
+        gradient_accumulation_steps=2,
+        gradient_checkpointing=True,
+        bf16=True,
+        tf32=True,
+        learning_rate=2e-4,
+        weight_decay=0.01,
+        warmup_ratio=0.1,
+        lr_scheduler_type="linear",
+        logging_steps=10,
+        eval_strategy="epoch",
+        save_strategy="epoch",
+        metric_for_best_model="precision_at_5",
+        load_best_model_at_end=True,
+    )
 
-    test_dataset = TensorDataset(X_test, y_test)
-    test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=False)
-    #Prevent test_set shuffling to maintain reproducibility and comprehension
 
+    trainer = Trainer(
+        model=model_physbert,
+        args=training_args,
+        train_dataset=tokenized_train,
+        eval_dataset=tokenized_test,
+        compute_metrics=compute_metrics,
+        data_collator=collator,
+    )
+
+    trainer.train()
     
-    # Data collator for dynamic padding
-    collator = DataCollatorWithPadding(tokenizer)
-
-    inputs = tokenizer(df["title"],
-                       df["abstract"],
-                       use_fast=True,
-                       truncation="only_second",
-                       max_length=512,
-                       return_tensors='pt')
-    
-    train_dataset = Dataset(inputs)
-
-    model_physbert.to(device)
-    criterion = nn.BCEWithLogitsLoss()
-    optimizer = torch.optim.AdamW(model_physbert.parameters(), lr=1e-3)
-
-    
-    logger.info("Starting training loop...")
-    for epoch in range(EPOCHS):
-
-        #Training
-        model_physbert.train()
-        train_loss = 0.0
-        logger.info(f"Epoch {epoch+1}/{EPOCHS} [Train]")
-        for batch_X, batch_y in tqdm(train_loader, desc=f"Epoch {epoch+1}/{EPOCHS} [Train]"):
-            batch_X, batch_y = batch_X.to(device), batch_y.to(device)
-            optimizer.zero_grad()
-            preds = model(batch_X)
-            loss = criterion(preds, batch_y)
-            loss.backward()
-            optimizer.step()
-
-            train_loss += loss.item() * batch_X.size(0)
-
-        #Validation
-        model_physbert.eval()
-        val_loss = 0.0
-        with torch.no_grad():
-            for batch_X, batch_y in test_loader:
-                batch_X, batch_y = batch_X.to(device), batch_y.to(device)
-                preds = model(batch_X)
-                loss = criterion(preds, batch_y)
-                val_loss += loss.item() * batch_X.size(0)
-
-        val_loss /= len(test_loader.dataset)
-        train_loss /= len(train_loader.dataset)
-
-        logger.info(f"Epoch {epoch+1:02d}/{EPOCHS:02d} | Train Loss: {train_loss:.4f} | Val Loss: {val_loss:.4f}")
-
-
-
-    torch.save(model_physbert.state_dict(), DATA_PATH / "models" / "physbert_lora_finetuned.pt")
+    #torch.save(model_physbert.state_dict(), DATA_PATH / "models" / "physbert_lora_finetuned.pt")
     
